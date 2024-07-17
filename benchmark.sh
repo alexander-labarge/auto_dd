e901447ff0c7c7ada9ee54e3caee799245f378b7
Alexander La Barge <alex@labarge.dev>
2024-07-16 22:14:58 -0400

    readme

--- benchmark.sh
+++ benchmark.sh
@@ -3,4 +3,4 @@
 source venv/bin/activate
 sudo venv/bin/python3.12 auto_dd.py --source /dev/nvme0n1 --destination /dev/sda --benchmark --benchmark-size 4096
 
-# sudo venv/bin/python3.12 auto_dd.py --source /dev/nvme0n1 --destination /dev/sda --enable-service --start-service --block-size 32768
+# sudo venv/bin/python3.12 auto_dd.py --source /dev/nvme0n1 --destination /dev/sda --start-service --block-size 32768
